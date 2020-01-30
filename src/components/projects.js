0f91f5d6af3f5be7d35eb6a71877a938f940cf48
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-01-30 16:25:36 -0500

    add skeleton styling and a fetchProjects() function

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,12 +2,18 @@ class Projects {
     constructor() {
         this.projects = []
         this.adapter = new projectsAdapter()
+        this.fetchProjects()
     }
 
     fetchProjects() {
         this.adapter.getProjects()
-        .then(projects => projects.data.forEach(project => {
-            this.projects.push(new Project(project)))
-        });
+        .then(projects => {
+            projects.data.forEach(project => this.projects.push(new Projects(project)))
+        })
+        .then(() => this.render())
+    }
+
+    render() {
+        
     }
 }
