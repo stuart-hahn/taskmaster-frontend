802a5e357cb02cc3109c8e2fcb6c1bb6eaeca763
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-08 09:59:47 -0500

    turn h1 into 'back' button of sorts

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,15 +12,23 @@ class Projects {
         this.tasksList = document.querySelector(".tasksList")
         this.newProjectForm = document.querySelector(".newProjectForm")
         this.newProjectButton = document.querySelector(".newProjectButton")
-        
+        this.returnHome = document.querySelector("#return-home")
     }
 
     initEventListeners() {
         this.projectsList.addEventListener('click', this.displayTasks.bind(this))
+        this.newProjectForm.addEventListener("submit", this.createProject.bind(this))
         this.newProjectButton.addEventListener('click', () => {
             this.newProjectButton.classList.toggle('hidden')
             this.newProjectForm.classList.toggle('hidden')
         })
+        this.returnHome.addEventListener('click', this.render.bind(this))
+    }
+
+    createProject(e) {
+        e.preventDefault()
+        this.newProjectButton.classList.toggle('hidden')
+        this.newProjectForm.classList.toggle('hidden')
     }
 
     fetchProjects() {
@@ -33,6 +41,12 @@ class Projects {
 
     render() {
         this.projectsList.innerHTML = ""
+        this.tasksList.innerHTML = ""
+        const projectsHeader = document.createElement('h2')
+        projectsHeader.classList.add('sectionHeader')
+        projectsHeader.innerText = "All Projects"
+        this.projectsList.appendChild(projectsHeader)
+
         this.projects.forEach(project => {
             const projectLi = document.createElement('li')
             projectLi.id = project.id
@@ -46,6 +60,7 @@ class Projects {
 
         this.projectsList.innerHTML = ""
         const projectTitle = document.createElement('h2')
+        projectTitle.classList.add('sectionHeader')
         projectTitle.innerText = project.attributes.title
         this.tasksList.appendChild(projectTitle)
         
@@ -54,5 +69,7 @@ class Projects {
             taskLi.innerText = task.title
             this.tasksList.appendChild(taskLi)
         })
+
+        this.newProjectButton.classList.toggle('hidden')
     }
 }
