05bc10132f76379801c3e36afdc414b8e2010960
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-08 12:38:19 -0500

    fix class names and styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,10 +8,10 @@ class Projects {
     }
 
     initBindings() {
-        this.projectsList = document.querySelector(".projectsList")
-        this.tasksList = document.querySelector(".tasksList")
-        this.newProjectForm = document.querySelector(".newProjectForm")
-        this.newProjectButton = document.querySelector(".newProjectButton")
+        this.projectsList = document.querySelector(".projects-list")
+        this.tasksList = document.querySelector(".tasks-list")
+        this.newProjectForm = document.querySelector(".new-project-form")
+        this.newProjectButton = document.querySelector(".new-project-button")
         this.returnHome = document.querySelector("#return-home")
     }
 
@@ -19,7 +19,6 @@ class Projects {
         this.projectsList.addEventListener('click', this.displayTasks.bind(this))
         this.newProjectForm.addEventListener("submit", this.createProject.bind(this))
         this.newProjectButton.addEventListener('click', () => {
-            this.newProjectButton.classList.toggle('hidden')
             this.newProjectForm.classList.toggle('hidden')
         })
         this.returnHome.addEventListener('click', this.render.bind(this))
@@ -27,8 +26,6 @@ class Projects {
 
     createProject(e) {
         e.preventDefault()
-        this.newProjectButton.classList.toggle('hidden')
-        this.newProjectForm.classList.toggle('hidden')
     }
 
     fetchProjects() {
@@ -43,7 +40,7 @@ class Projects {
         this.projectsList.innerHTML = ""
         this.tasksList.innerHTML = ""
         const projectsHeader = document.createElement('h2')
-        projectsHeader.classList.add('sectionHeader')
+        projectsHeader.classList.add('section-header')
         projectsHeader.innerText = "All Projects"
         this.projectsList.appendChild(projectsHeader)
 
@@ -60,7 +57,7 @@ class Projects {
 
         this.projectsList.innerHTML = ""
         const projectTitle = document.createElement('h2')
-        projectTitle.classList.add('sectionHeader')
+        projectTitle.classList.add('section-header')
         projectTitle.innerText = project.attributes.title
         this.tasksList.appendChild(projectTitle)
         
@@ -69,7 +66,5 @@ class Projects {
             taskLi.innerText = task.title
             this.tasksList.appendChild(taskLi)
         })
-
-        this.newProjectButton.classList.toggle('hidden')
     }
 }
